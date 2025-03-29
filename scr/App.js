4e7c1faafb6d37a1bc68a881dfce498c569e4250
IndhiraSivasakthi <147968696+IndhiraSivasakthi@users.noreply.github.com>
2025-03-29 12:57:28 +0530

    Add files via upload

--- scr/App.js
+++ scr/App.js
@@ -1,1 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import UserList from "./components/UserList";
+import EditUser from "./components/EditUser";
+import PrivateRoute from "./components/PrivateRoute";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/users" element={<PrivateRoute><UserList /></PrivateRoute>} />
+        <Route path="/edit/:id" element={<PrivateRoute><EditUser /></PrivateRoute>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
